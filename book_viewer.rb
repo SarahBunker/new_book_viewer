9ac14879da847fc88b401062335f3d28f8d1f8f0
Sarah Bunker <ChemEnCoder@gmail.com>
2022-05-27 13:33:43 -0600

    adding chapters routes, adding before method, adding helper methods

--- book_viewer.rb
+++ book_viewer.rb
@@ -2,9 +2,33 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 
+before do
+  @contents = File.readlines("data/toc.txt")
+end
+
+
+helpers do
+
+  def in_paragraphs(text)
+    text.split("\n\n").map do |paragraph|
+      "<p>#{paragraph}</p>"
+    end.join
+  end
+
+end
+
 get "/" do
   @title = "The Adventures of Sherlock Holmes"
-  @contents = File.readlines("data/toc.txt")
 
   erb :home
+end
+
+get "/chapters/:number" do
+  number = params[:number].to_i
+  chapter_name = @contents[number - 1]
+  @title = "Chapter #{number}: #{chapter_name}"
+
+  @chapter = File.read("data/chp#{number}.txt")
+
+  erb :chapter
 end
